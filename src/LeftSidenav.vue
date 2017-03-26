886d18a939d3421b130003a23f9efb60fb68d021
tyeon95 <wony_95@hotmail.com>
2017-03-27 06:15:34 +0700

    remove

--- src/LeftSidenav.vue
+++ src/LeftSidenav.vue
@@ -5,9 +5,6 @@
         <md-icon>menu</md-icon>
       </md-button>
       <h2 class="md-title">Monster Clicker</h2>
-      <md-button class="md-icon-button" @click.native="logout">
-        Bye
-      </md-button>
     </md-toolbar>
     <md-sidenav class="md-left" ref="leftSidenav">
       <md-toolbar>
@@ -85,8 +82,6 @@
 </template>
 
 <script>
-import UsersApi from './api/users.js'
-import router from './router'
 
 export default {
   methods: {
@@ -96,12 +91,6 @@ export default {
     buyWeapon () {
       console.log('BUY')
       this.gold -= 10
-    },
-    logout () {
-      UsersApi.logout(this.email, this.password, function (_response) {
-        // NOTE: Posts.index is not yet implemented
-        if (_response === true) router.push({ name: 'Users.sign_in' })
-      })
     }
   }
 }
